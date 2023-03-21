6c57cc75a6db10fad59db7e6ba7505d413016f7c
Clément <simon.clement.pierre.daniel@gmail.com>
2023-03-21 17:41:53 +0100

    Restore skeleton project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
-import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainComponent,
-    NavComponent,
   ],
   imports: [ BrowserModule ],
   bootstrap: [ AppComponent ]
